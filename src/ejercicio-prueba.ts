e073d6bba3bd7a885f50fc0c652f3c805852ab75
Óscar Pozo Fernández <alu0101036526@ull.edu.es>
2021-04-11 23:16:53 +0100

    Fichero inicializado

---
+++ src/ejercicio-prueba.ts
@@ -0,0 +1,16 @@
+/* eslint-disable valid-jsdoc */
+/**
+ * Ejercicio de pruebas para la práctica 7.
+ */
+
+/**
+ * Función
+ * @param primerNum 1
+ * @param segundoNum 2
+ * @return la suma
+ */
+function addTwoNumbers(primerNum: number, segundoNum: number): number {
+  return primerNum + segundoNum;
+}
+
+console.log(addTwoNumbers(1, 2));
